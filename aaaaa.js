2d785ee0be50a74a1b72cbf30c2f27da7303e2fd
Md.Tanvir Ahmmed <63459867+tanvirahmmed8@users.noreply.github.com>
2022-01-30 22:42:06 +0600

    Frist User

---
+++ aaaaa.js
@@ -0,0 +1,12 @@
+  document.querySelector('#runCoDE').addEventListener('click', onclick, false)
+
+    function onclick() {
+        chrome.tabs.query({
+                currentWindow: true,
+                active: true
+            },
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, 'Start Program in 15 seconds "Press OK"')
+            }
+        )
+    }
