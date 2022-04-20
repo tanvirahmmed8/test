882a53d66de8d9394a3686e2dadde94b6f9688e7
Md.Tanvir Ahmmed <63459867+tanvirahmmed8@users.noreply.github.com>
2022-04-20 15:00:45 +0600

    Update poup.js

--- poup.js
+++ poup.js
@@ -7,7 +7,7 @@
                 active: true
             },
             function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, 'Start Program in 15 seconds "Press OK"')
+                chrome.tabs.sendMessage(tabs[0].id, 'এই সফটওয়্যার টি বন্ধ হয়ে যাবে "Press OK"')
             }
         )
     }
